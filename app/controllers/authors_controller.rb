e54b45e5b78f2988d5b5b7a805b3d49715ff6823
logicfirst <slee@logicfirstllc.com>
2020-12-28 17:41:53 -0500

    Completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,13 +4,25 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
-
+    @author = Author.create(author_params)
+    if @author.valid?
+      @author.save
     redirect_to author_path(@author)
+    else render :new
+    end
   end
+  # @person = Person.new(person_params)
+
+  #   if @person.valid?
+  #     @person.save
+  #     redirect_to person_path(@person)
+  #   else
+  #     # re-render the :new template WITHOUT throwing away the invalid @person
+  #     render :new
 
   private
 
